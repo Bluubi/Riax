e79be5d23bbc1bc15e513354f4477bc02b438364
BluubiDev <bluubi.dev@gmail.com>
2024-03-09 15:02:14 +0100

    stylish

--- src/app/features/user-profile/application/form-image-profile/index.tsx
+++ src/app/features/user-profile/application/form-image-profile/index.tsx
@@ -9,11 +9,12 @@ type FormImageProfile = ImageProfileProps & {
 export const FormImageProfile = ({image, dispatchForm, dispatchEvent}: FormImageProfile) => {
 
     return (
-             <div className={`fixed flex justify-center items-center bg-black bg-opacity-50 inset-0 ${dispatchForm ? 'visible' : 'invisible'}`} onClick={() => dispatchEvent()}>
+        <div className={`fixed flex flex-col justify-center items-center bg-black bg-opacity-50 inset-0 ${dispatchForm ? 'visible' : 'invisible'}`} onClick={() => dispatchEvent()}>
             <IoMdClose className={'absolute top-0 right-0 m-4'} fill={'white'} onClick={() => dispatchEvent()}/>
             <div>
                 {image && <Image src={image} alt={'Profile image'}/>}
             </div>
+                 <button className={'bg-gray-800 text-cyan-50 p-2 w-1/5 mt-2 rounded-sm'} data-testid={'button-form'}> Update image </button>
         </div>
 
 )
